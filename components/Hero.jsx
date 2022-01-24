0fe92266f965ab365576453d4db7f121407267c6
Laud Gilbert <laudaccurate@gmail.com>
2022-01-24 08:58:39 +0000

    navbar and theme dones

---
+++ components/Hero.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
+import { RainbowHighlight } from "./RainbowHighlight";
+
+export default function Hero() {
+  const colors = ["#F59E0B", "#84CC16", "#10B981", "#3B82F6"];
+  return (
+    <div className="flex flex-row justify-center items-start overflow-hidden">
+      {/* Text container */}
+
+      <div className="w-full md:w-1/2 mx-auto text-center md:text-left lg:p-20">
+        <RoughNotationGroup show={true}>
+          <RainbowHighlight color={colors[0]}>
+            <h1 className="text-4xl md:text-8xl font-bold text-gray-700 dark:text-gray-200 my-2">
+              Developer.
+            </h1>
+          </RainbowHighlight>
+       </RoughNotationGroup>
+     </div>
+    </div>
+  );
+}
